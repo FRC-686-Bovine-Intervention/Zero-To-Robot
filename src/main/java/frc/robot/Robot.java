cad60fbb30f51d4cac3b929dda299a989726da82
LightspeedLazer <michael@the-sims-family.net>
2024-12-18 17:42:24 -0500

    Akit setup

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,58 +4,153 @@
 
 package frc.robot;
 
-import org.littletonrobotics.junction.LoggedRobot;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class Robot extends LoggedRobot {
-  private final RobotContainer m_robotContainer;
+    
+    public Robot() {
+        aKitSetup();
+    }
 
-  public Robot() {
-    m_robotContainer = new RobotContainer();
-  }
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
 
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
+    @Override
+    public void disabledInit() {}
 
-  @Override
-  public void disabledInit() {}
+    @Override
+    public void disabledPeriodic() {}
 
-  @Override
-  public void disabledPeriodic() {}
+    @Override
+    public void disabledExit() {}
 
-  @Override
-  public void disabledExit() {}
+    @Override
+    public void autonomousInit() {}
 
-  @Override
-  public void autonomousInit() {}
+    @Override
+    public void autonomousPeriodic() {}
 
-  @Override
-  public void autonomousPeriodic() {}
+    @Override
+    public void autonomousExit() {}
 
-  @Override
-  public void autonomousExit() {}
+    @Override
+    public void teleopInit() {}
 
-  @Override
-  public void teleopInit() {}
+    @Override
+    public void teleopPeriodic() {}
 
-  @Override
-  public void teleopPeriodic() {}
+    @Override
+    public void teleopExit() {}
 
-  @Override
-  public void teleopExit() {}
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
 
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
+    @Override
+    public void testPeriodic() {}
 
-  @Override
-  public void testPeriodic() {}
+    @Override
+    public void testExit() {}
 
-  @Override
-  public void testExit() {}
+    private void aKitSetup() {
+        System.out.println("[Init Robot] Recording AdvantageKit Metadata");
+        Logger.recordMetadata("Robot", RobotType.getRobot().name());
+        Logger.recordMetadata("Mode", RobotType.getMode().name());
+        Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+        Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+        Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+        Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+        Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+        Logger.recordMetadata("GitDirty", 
+            switch(BuildConstants.DIRTY) {
+                case 0 -> "All changes committed";
+                case 1 -> "Uncomitted changes";
+                default -> "Unknown";
+            }
+        );
+
+        // Set up data receivers & replay source
+        System.out.println("[Init Robot] Configuring AdvantageKit for " + RobotType.getMode().name() + " Zero-To-Robot" + RobotType.getRobot().name());
+        switch (RobotType.getMode()) {
+            case REAL: // Running on a real robot, log to a USB stick
+                Logger.addDataReceiver(new WPILOGWriter("/media/sda1/"));
+                Logger.addDataReceiver(new NT4Publisher());
+            break;
+            case SIM: // Running a physics simulator, log to local folder
+                Logger.addDataReceiver(new WPILOGWriter("logs"));
+                Logger.addDataReceiver(new NT4Publisher());
+            break;
+            case REPLAY: // Replaying a log, set up replay source
+                setUseTiming(false); // Run as fast as possible
+                String logPath = LogFileUtil.findReplayLog();
+                Logger.setReplaySource(new WPILOGReader(logPath));
+                Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+            break;
+        }
+
+        System.out.println("[Init Robot] Starting AdvantageKit");
+        Logger.start();
+
+        System.out.println("[Init Robot] Starting Command Logger");
+        Map<String, Integer> commandCounts = new HashMap<>();
+        BiConsumer<Command, Boolean> logCommandFunction =
+        (Command command, Boolean active) -> {
+            String name = command.getName();
+            int count = commandCounts.getOrDefault(name, 0) + (active ? 1 : -1);
+            commandCounts.put(name, count);
+            // Logger.recordOutput(
+            //         "Commands/Unique/" + name + "_" + Integer.toHexString(command.hashCode()), active.booleanValue());
+            // if(command.getRequirements().size() == 0) {
+            //   Logger.recordOutput("Commands/No Requirements/" + name, count > 0);
+            // }
+            for(Subsystem subsystem : command.getRequirements()) {
+                Logger.recordOutput("Commands/" + subsystem.getName(), (count > 0 ? name : "none"));
+                // Logger.recordOutput("Commands/" + subsystem.getName() + "/" + name, count > 0);
+            }
+        };
+
+        CommandScheduler.getInstance()
+            .onCommandInitialize(
+                (Command command) -> {
+                    logCommandFunction.accept(command, true);
+                }
+            )
+        ;
+        CommandScheduler.getInstance()
+            .onCommandFinish(
+                (Command command) -> {
+                    logCommandFunction.accept(command, false);
+                }
+            )
+        ;
+        CommandScheduler.getInstance()
+            .onCommandInterrupt(
+                (Command command) -> {
+                    logCommandFunction.accept(command, false);
+                }
+            )
+        ;
+
+        System.out.println("[Init Robot] Instantiating RobotContainer");
+        new RobotContainer();
+
+        SmartDashboard.putData("Command Scheduler", CommandScheduler.getInstance());
+    }
 }
---
+++ src/main/java/frc/robot/RobotType.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+public enum RobotType {
+    ROBOT_2024_COMP,
+    ROBOT_2024_PRAC,
+    ;
+    private static final boolean isReplay = true;
+    public static Mode getMode() {
+        return Robot.isReal() ? Mode.REAL : (isReplay ? Mode.REPLAY : Mode.SIM);
+    }
+    public static RobotType getRobot() {
+        return getMode().defaultRobotType;
+    }
+
+    public static enum Mode {
+        REAL    (RobotType.ROBOT_2024_PRAC),
+        SIM     (RobotType.ROBOT_2024_COMP),
+        REPLAY  (RobotType.ROBOT_2024_PRAC),
+        ;
+        private final RobotType defaultRobotType;
+        Mode(RobotType defaultType) {
+            this.defaultRobotType = defaultType;
+        }
+    }
+}
